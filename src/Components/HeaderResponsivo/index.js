ee8b6a4236771be5e5667e90c816d3e196a290b0
Amandasantana24 <74264048+Amandasantana24@users.noreply.github.com>
2021-11-03 16:28:11 -0400

    Adicionando pacote de icones do Fonte Awesome

---
+++ src/Components/HeaderResponsivo/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./styles.css";
+
+function HeaderResponsivo (){
+    const [active, setActive] = useState(false)
+
+    function openMenu(event) {
+        event.preventDefault();
+        setActive(!active);
+        console.log(active);
+    }
+  return (
+      <header className = "header">
+          <div className="menu-hamburguer" onClick={openMenu}>
+              <FontAwesomeIcon icon={faBars} color="#f4f4f4" size="lg"/>
+          </div>
+          <ul className = {`menu-options${active ? ' active' : ''} `}>
+             <li><a href="/">Home</a></li> 
+             <li><a href="/">Personagens</a></li> 
+             <li><a href="/">Epsódios</a></li> 
+             <li><a href="/">Sobre</a></li> 
+          </ul>
+      </header>
+  )
+
+}
+export default HeaderResponsivo;
