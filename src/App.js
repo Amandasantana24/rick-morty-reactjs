2c91b697c2eaf72b43a654790ffbc7e10c88970f
Amandasantana24 <74264048+Amandasantana24@users.noreply.github.com>
2021-11-03 21:08:26 -0400

    Arrumando push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import HeaderResponsive fron './Components/HeaderResponsive';
-
 function App() {
   return (
     <div className= "App">
       <header className="App-header">
-      
+
         Criando a pragina principal
         
         <HeaderResponsive/>
