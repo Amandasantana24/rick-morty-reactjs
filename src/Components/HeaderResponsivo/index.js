101a0e466d74fc7c5d481bfaf35152532b172a85
Amandasantana24 <74264048+Amandasantana24@users.noreply.github.com>
2021-11-05 17:26:29 -0400

    Corrigindo Componentes

--- src/Components/HeaderResponsivo/index.js
+++ src/Components/HeaderResponsivo/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import React from "react";
 import "./styles.css";
 
 function HeaderResponsivo (){
