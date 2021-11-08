8cd783ee64e416df986fa3d01438d5640ba95250
Amandasantana24 <74264048+Amandasantana24@users.noreply.github.com>
2021-11-08 10:06:35 -0400

    Chamando o componente principal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderResponsivo from './Components/HeaderResponsivo';
+import MainSection from './Components/MainSection';
 
 function App() {
   return (
     <div className= "App">
       <HeaderResponsivo/>
+      <MainSection/>
     </div>
   );
 }
